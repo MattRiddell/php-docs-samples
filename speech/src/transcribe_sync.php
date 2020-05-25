69ac6d46ddf8cff2da1abef8934c909b7c0b13cf
Matthew Riddell <matt@createoffshorecompany.com>
2020-05-25 16:20:03 -0500

    change

--- speech/src/transcribe_sync.php
+++ speech/src/transcribe_sync.php
@@ -69,7 +69,7 @@ try {
         $confidence = $mostLikely->getConfidence();
         printf('Transcript: %s' . PHP_EOL, $transcript);
         $result = Array();
-        $result['result'] = htmlentities(stripslashes(utf8_encode($transcript)), ENT_QUOTES);;
+        $result['result'] = (utf8_encode($transcript);
         echo json_encode($result);
         break;
         // printf('Confidence: %s' . PHP_EOL, $confidence);
